724bc7ad318be3f73422659e1adcb8858ddb8f0e
plr1993 <pablolope93@gmail.com>
2022-05-27 14:00:27 +0200

    ecoo

--- routes/eco.js
+++ routes/eco.js
@@ -1,39 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var unirest = require('unirest');
 //Peticion de cambio de contraseña. Se llamará cuando alguien no recuerde su contraseña
-router.get('/',function(){
-    var https = require('follow-redirects').https;
-    var fs = require('fs');
-    
-    var options = {
-      'method': 'GET',
-      'hostname': 'fortnite-api.com',
-      'path': '/v2/stats/br/v2?name=EcoslavTwitch&image=keyboardMouse',
-      'headers': {
-        'Authorization': 'b6b08a09-6d58-4e1c-9d90-74f3e0cdf718'
-      },
-      'maxRedirects': 20
-    };
-    
-    var req = https.request(options, function (res) {
-      var chunks = [];
-    
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
-    
-      res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-      });
-    
-      res.on("error", function (error) {
-        console.error(error);
-      });
-    });
-    
-    req.end();
+router.get('/',function(req, res){
+  var request = require('request');
+  var options = {
+    'method': 'GET',
+    'url': 'https://fortnite-api.com/v2/stats/br/v2?name=EcoslavTwitch&image=keyboardMouse',
+    'headers': {
+      'Authorization': 'b6b08a09-6d58-4e1c-9d90-74f3e0cdf718'
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.send("Ecoslav ha ganado "+JSON.parse(response.body).data.stats.keyboardMouse.overall.wins+" partidas en Fortnite");
+  });
+  
 });
 
 module.exports = router; 
